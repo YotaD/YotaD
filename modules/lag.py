8b1602c87f73418d0537b46da5a88dc57505c50f
linel18 <32824135+linel18@users.noreply.github.com>
2018-04-22 22:46:09 -0400

    Add files via upload

--- modules/lag.py
+++ modules/lag.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python
+"""
 
+lag.py - Yota lag module
+"""
+
+
+from time import time
+
+def lag(m5, input):
+    nick = input.group(2)
+    lag = str(time())
+    ctcp = "\x01PING %s\x01" % lag
+    m5.write(["PRIVMSG", nick, ctcp])
